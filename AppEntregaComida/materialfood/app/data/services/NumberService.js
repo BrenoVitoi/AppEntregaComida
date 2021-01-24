7b79f9ca18831e7125a3dccc104942d4ef995b21
Breno Souza <breno.vitoi@gmail.com>
2021-01-24 16:10:21 -0300

    Implementamos o ProductsView.js

--- AppEntregaComida/materialfood/app/data/services/NumberService.js
+++ AppEntregaComida/materialfood/app/data/services/NumberService.js
@@ -1,9 +1,13 @@
+import nmeral from 'numeral';
 export const NumberService = {
     currency(value) {
-        const formatter =  new Intl.NumberFormat('pt-BR', {
+      /*  const formatter =  new Intl.NumberFormat('pt-BR', {
             style: 'currency',
             currency: 'BRL'
         })
-        return formatter.format(value);
+        return formatter.format(value); */
+            /*return numeral(value).format('0.0[,]00'); */
+            return 'R$ ' + numeral(value).format('0.0,00');
+
     }
 }
